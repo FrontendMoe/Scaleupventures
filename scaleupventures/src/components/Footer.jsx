ef415ad19988a427baf7fd9664fc555f4c9e43e5
frontendmoe <frontendmoe@gmail.com>
2023-11-03 00:02:54 +0100

    Fixes

--- scaleupventures/src/components/Footer.jsx
+++ scaleupventures/src/components/Footer.jsx
@@ -1,37 +1,36 @@
 function Footer() {
-  return (
-    <footer className="bg-black py-[80px] lg:py-[300px] padding space-y-[80px] text-center text-[#F2F3FF] w-full">
-      <div className="space-y-[40px]">
-        <p className="text-[30px] lg:text-[35px] font-[400] leading-[34px] lg:leading-[26px] tracking-[-1.05px]">
-          Invest with ScaleUp Ventures
-        </p>
-        <p className="text-[15px] font-[500] mx-auto max-w-[817px] leading-[26px] ">
-          Partner with a dedicated and experienced team with a clear vision for
-          success. <br />
-          <br /> Our strategic approach ensures maximized returns, making
-          ScaleUp Ventures a prime choice for partnering.
-        </p>
-      </div>
-      <p className="text-[17.39px] font-[500] leading-[26px] ">
-        Interested in partnering with ScaleUp Ventures and tapping into a unique
-        market opportunity <br /> enriched with exclusive IP? <br />
-        <br />
-        <span className="font-bold">
-          Contact us to explore collaboration and investment possibilities.
-        </span>
-      </p>
-      <div>
-        <a
-          href="mailto:deals@scaleupventures.co"
-          className="text-[17.39px] font-[500] leading-[26px] "
-        >
-          Contact Us: <br />
-          <br />
-          deals@scaleupventures.co
-        </a>
-      </div>
-    </footer>
-  )
+	return (
+		<footer className='bg-black py-[80px] lg:py-[300px] padding space-y-[80px] text-center text-[#F2F3FF] w-full'>
+			<div className='space-y-[40px]'>
+				<p className='text-[30px] lg:text-[35px] font-[400] leading-[34px] lg:leading-[26px] tracking-[-1.05px]'>
+					Invest with <span className='font-bold'>ScaleUp Ventures</span>
+				</p>
+				<p className='text-[15px] font-[500] mx-auto max-w-[817px] leading-[26px] '>
+					Partner with a dedicated and experienced team with a clear vision for
+					success. <br />
+					<br /> Our strategic approach ensures maximized returns, making
+					ScaleUp Ventures a prime choice for partnering.
+				</p>
+			</div>
+			<p className='text-[17.39px] font-[500] leading-[26px] '>
+				Interested in partnering with ScaleUp Ventures and tapping into a unique
+				market opportunity <br /> enriched with exclusive IP? <br />
+				<br />
+				<span className='font-bold'>
+					Contact us to explore collaboration and investment possibilities.
+				</span>
+			</p>
+			<div>
+				<a
+					href='mailto:deals@scaleupventures.co'
+					className='text-[17.39px] font-[500] leading-[26px] '>
+					Contact Us: <br />
+					<br />
+					deals@scaleupventures.co
+				</a>
+			</div>
+		</footer>
+	);
 }
 
-export default Footer
+export default Footer;
